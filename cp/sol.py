dcbf3f04aebab1738ed727f5f88cdf8a3b4d921b
ibrahimM <ibrahimmustafa787898@gmail.com>
2025-11-04 16:48:34 -0500

    modified sol.py

--- cp/sol.py
+++ cp/sol.py
@@ -4,40 +4,40 @@
 
 
 # Competitive Programming version
-# import sys
+import sys
 
-# input = lambda: sys.stdin.readline().rstrip()
-# print = lambda *args, **kwargs: sys.stdout.write(' '.join(map(str, args)) + '\n')
+input = lambda: sys.stdin.readline().rstrip()
+print = lambda *args, **kwargs: sys.stdout.write(' '.join(map(str, args)) + '\n')
 
-# def main():
-#     n, target = map(int, input().split())
-#     nums = list(map(int, input().split()))
+def main():
+    n, target = map(int, input().split())
+    nums = list(map(int, input().split()))
 
-#     seen = {}
+    seen = {}
 
-#     for i, num in enumerate(nums):
-#         complement = target - num
-#         if complement in seen:
-#             print(seen[complement], i)
-#             return
-#         seen[num] = i
+    for i, num in enumerate(nums):
+        complement = target - num
+        if complement in seen:
+            print(seen[complement], i)
+            return
+        seen[num] = i
 
-# if __name__ == "__main__":
-#     main()
+if __name__ == "__main__":
+    main()
 
 
 # LeetCode version
-class Solution:
-    def twoSum(self, nums: list[int], target: int) -> list[int]:
-        seen = {}
-        for i, num in enumerate(nums):
-            complement = target - num
-            if complement in seen:
-                return [seen[complement], i]
-            seen[num]  = i
-        return []   
+# class Solution:
+#     def twoSum(self, nums: list[int], target: int) -> list[int]:
+#         seen = {}
+#         for i, num in enumerate(nums):
+#             complement = target - num
+#             if complement in seen:
+#                 return [seen[complement], i]
+#             seen[num]  = i
+#         return []   
 
-if __name__ == "__main__":
-    sol = Solution()
-    res = sol.twoSum([3,2,4], 6)
-    print(*res)
+# if __name__ == "__main__":
+#     sol = Solution()
+#     res = sol.twoSum([3,2,4], 6)
+#     print(*res)
